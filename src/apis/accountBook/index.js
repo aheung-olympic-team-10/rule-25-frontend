d3a7ce643f57678676cf0ef0a5954428c1850d3b
devHudi <devhudi@gmail.com>
2021-09-25 12:22:44 +0900

    feat: implement account book api

---
+++ src/apis/accountBook/index.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const URL = `${process.env.REACT_APP_API_HOST}/account-books`;
+
+export default {
+  create: (userId, type, category, date, content, amount) =>
+    axios({
+      url: `${URL}/`,
+      method: 'POST',
+      data: { userId, type, category, date, content, amount },
+      headers: { authorization: `Bearer ${localStorage.getItem('jwt')}` },
+    }),
+  update: (id, type, category, date, content) =>
+    axios({
+      url: `${URL}/${id}`,
+      method: 'PUT',
+      data: { type, category, date, content },
+      headers: { authorization: `Bearer ${localStorage.getItem('jwt')}` },
+    }),
+  delete: (id) =>
+    axios({
+      url: `${URL}/${id}`,
+      method: 'DELETE',
+      headers: { authorization: `Bearer ${localStorage.getItem('jwt')}` },
+    }),
+};
